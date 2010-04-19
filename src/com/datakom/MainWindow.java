2975cf6a066ed2fd1735d742be3c3a292b5085f0
burbas <burbas@192.168.100.104>
2010-04-19 16:39:58 +0200

    Added Settings-tab to the application

--- src/com/datakom/MainWindow.java
+++ src/com/datakom/MainWindow.java
@@ -12,6 +12,23 @@ public class MainWindow extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Create the first tab
+        Resources res = getResources();
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec spec;
+        Intent intent;
+        
+        // Creates an intent that can be reusable
+        intent = new Intent().setClass(this, TabSettings.class);
+        
+        
+        spec = tabHost.newTabSpec("settings").setIndicator("Settings", 
+        		res.getDrawable(R.drawable.ic_tab_settings))
+        		.setContent(intent);
+       tabHost.addTab(spec);
+        	
+        
 
     }
 }
