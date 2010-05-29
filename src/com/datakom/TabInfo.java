cfc7f0a05bc7b79e03472b81d50b86880f743ae6
toxilox <toxilox@gmail.com>
2010-05-29 14:40:47 +0200

    massor
    Signed-off-by: toxilox <toxilox@gmail.com>

--- src/com/datakom/TabInfo.java
+++ src/com/datakom/TabInfo.java
@@ -1,16 +1,15 @@
 package com.datakom;
 
 
-import com.datakom.POIObjects.HaggleConnector;
-import com.datakom.POIObjects.ObjectTypes;
-import com.datakom.POIObjects.POIObject;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.datakom.POIObjects.HaggleConnector;
 
 public class TabInfo extends Activity {
 	HaggleConnector conn;
@@ -19,6 +18,38 @@ public class TabInfo extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tabinfoview);
 		
-   
+		TableLayout tl = (TableLayout) findViewById(R.id.comment_table);
+		TextView title = (TextView) findViewById(R.id.info_title);
+		ImageView star1 = (ImageView) findViewById(R.id.star_1);
+		ImageView star2 = (ImageView) findViewById(R.id.star_2);
+		ImageView star3 = (ImageView) findViewById(R.id.star_3);
+		ImageView star4 = (ImageView) findViewById(R.id.star_4);
+		ImageView star5 = (ImageView) findViewById(R.id.star_5);
+		ImageView iv = (ImageView) findViewById(R.id.poi_img);
+		iv.setImageResource(R.drawable.btn_rating_star_off_selected);
+		star1.setImageResource(R.drawable.rate_star_big_on);
+		star2.setImageResource(R.drawable.rate_star_big_on);
+		star3.setImageResource(R.drawable.rate_star_big_on);
+		star4.setImageResource(R.drawable.rate_star_big_half);
+		star5.setImageResource(R.drawable.rate_star_big_off);
+		title.setText("Title");
+		tl.addView(makeTableRow("höhö, lool", 4));
+		tl.addView(makeTableRow("höhö, bra", 4));
+		tl.addView(makeTableRow("höhö, bajs", 4));
+		
+	}
+	protected TableRow makeTableRow(String comment, int rating){
+		TextView commentView = new TextView(TabInfo.this);
+		commentView.setText(comment);
+		
+		TextView ratingView = new TextView(TabInfo.this);
+		ratingView.setText("dinmamma");
+		ratingView.setGravity(0x05);
+		
+		TableRow tr = new TableRow(TabInfo.this);
+		tr.addView(commentView);
+		tr.addView(ratingView);
+		
+		return tr;
 	}
 }
