38adfc56cec4114f761998e0138ffdb7f3908deb
toxilox <toxilox@gmail.com>
2010-05-31 15:46:19 +0200

    lagad...

--- src/com/datakom/TabSearch.java
+++ src/com/datakom/TabSearch.java
@@ -3,6 +3,7 @@ package com.datakom;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -11,7 +12,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.datakom.POIObjects.HaggleConnector;
-import com.datakom.POIObjects.POIObject;
 
 public class TabSearch extends Activity {
 	HaggleConnector conn = HaggleConnector.getInstance();
@@ -21,8 +21,11 @@ public class TabSearch extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    
 	    setContentView(R.layout.search_main);
+	    allNames = new ArrayList<String>();
+	    allNames.add("din mamma");
+	    allNames.add("din ma");
+	    
 	    
 	    final AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autoComp);
 	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.search_results, allNames);
@@ -31,20 +34,18 @@ public class TabSearch extends Activity {
 	    final Button button = (Button) findViewById(R.id.searchButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(allNames.contains(textView.getText().toString())){
-                	loadResult(textView.getText().toString());
+                if(allNames!=null && allNames.contains(textView.getText().toString())){
+                	Intent infoView = new Intent(TabSearch.this, TabInfo.class);
+                	infoView.putExtra("SEARCH_TITLE", textView.getText().toString());
+                	startActivity(infoView);
                 }
                 else{
-                	Toast.makeText(TabSearch.this, "No match", Toast.LENGTH_SHORT).show();
+                	Toast newToast = Toast.makeText(TabSearch.this, "no match.", Toast.LENGTH_SHORT);
+                	newToast.setGravity(48, 0, 60);
+                	newToast.show();
                 }
             }
         });
 
 	}
-	
-	protected void loadResult(String input){
-		POIObject result = conn.getPOIObjectByName(input);
-		
-	}
-
 }
